ff944c251ccd9c3521d21476d24a9eb08e76d8d0
Joseph Choi <josephchoi@josephs-mbp.home>
2023-12-31 23:56:59 -0500

    updated command line interface

--- main.cpp
+++ main.cpp
@@ -8,13 +8,15 @@
 int Node::m = 4;
 
 int main() {
+    std::cout << "Welcome to the Quaternary Space Partitioning Tree. " << std::endl;
+
     double user_x0;
     double user_y0;
     double user_x1;
     double user_y1;
 
     // prompt user for points
-    std::cout << "enter your test boundary" << std::endl;
+    std::cout << "First, enter your boundary points" << std::endl;
     std::cout << "x0: ";
     std::cin >> user_x0;
     std::cout << "y0: ";
@@ -27,38 +29,43 @@ int main() {
     Node* head = new Node(user_x0, user_y0, user_x1, user_y1);
     Quadtree tree = Quadtree(head);
 
+    std::string stop = "EXIT";
+    std::string inp_str = "";
 
+    while (inp_str.compare("EXIT") != 0) {
+        std::cout << "Enter 'I' to insert points, 'S' to search if a point exists, 'EXIT' to exit: ";
+        std::cin >> inp_str;
 
-    double user_x;
-    double user_y;
-    char cont;
-    while (true) {
-        std::cout << "enter x: ";
-        std::cin >> user_x;
-        std::cout << "enter y: ";
-        std::cin >> user_y;
+        if (inp_str.compare("I") == 0) {
+            double user_x;
+            double user_y;
+            char cont;
+            while (cont != 'n') {
+                std::cout << "enter x: ";
+                std::cin >> user_x;
+                std::cout << "enter y: ";
+                std::cin >> user_y;
 
-        if (user_x < user_x0 || user_y < user_y0 || user_x > user_x1 || user_y > user_y1)
-        {
-            std::cout << "outside boundary. quitting program" << std::endl;
-            return 1;
+                if (user_x < user_x0 || user_y < user_y0 || user_x > user_x1 || user_y > user_y1) {
+                    std::cout << "outside boundary. quitting program" << std::endl;
+                    return 1;
+                }
+
+                tree.insert(tree.get_head(), user_x, user_y);
+
+                std::cout << "continue? ";
+                std::cin >> cont;
+                std::cout << std::endl;
+            }
         }
-
-        tree.insert(tree.get_head(), user_x, user_y);
-
-        std::cout << "continue? ";
-        std::cin >> cont;
-        std::cout << std::endl;
-
-        if (cont == 'n')
-        {
-            break;
+        else if (inp_str.compare("S") == 0) {
+            std::cout << "Searching is still in production. Please try again later. " << std::endl;
         }
-
     }
 
-    
-
     tree.print(tree.get_head());
+
+    std::cout << std::endl;
+    std::cout << "end of program" << std::endl;
     return 0;
 }
